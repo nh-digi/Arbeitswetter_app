6bc82ccacb35f7033b37985b7149926ee39d328b
Yemima Fink <yemima.fink@neueshandeln.de>
2026-06-16 11:51:58 +0200

    Refactor code structure for improved readability and maintainability

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/arbeitswetter/sw.js', { scope: '/arbeitswetter/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Arbeitswetter_app/sw.js', { scope: '/Arbeitswetter_app/' })})}
